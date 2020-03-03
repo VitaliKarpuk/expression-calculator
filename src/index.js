ecf66d4130d1fcd383fc51728f2f080179663d98
Vitali Karpuk <karpuk-job@yandex.ru>
2020-03-03 22:15:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,32 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  const arrExpr = expr.split('')
+  const solidExpression = expr.match(/\S/g).join('')
+  const checkBrekets = (arr) => {
+    let summBrekets = 0;
+    for(let i =  0; i < arr.length; i++){
+      if(arr[i] === '('){
+        summBrekets +=1
+      }if(arr[i] === ')'){
+        summBrekets -= 1
+      }
+    }
+    if(summBrekets !== 0){
+      throw new Error("ExpressionError: Brackets must be paired.")
+    }
+  }
+  const checkZero = (arr) => {
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i] === '/' && arr[i+2] === '0' ){
+        throw new Error("TypeError: Division by zero.");
+      }
+    }
+  }
+  checkZero(arrExpr)
+  checkBrekets(arrExpr)
+  const result = new Function("return " + solidExpression)
+  return result()
 }
 
 module.exports = {
